41680a3b2335ae67461b2d1de4abfb4b57d833f4
Moisés R. Martins <90099815+moisesrmartins@users.noreply.github.com>
2022-10-03 18:17:26 -0300

    React_6_List Parte 1: Ajustes e preparações

--- list/src/index.js
+++ list/src/index.js
@@ -1,15 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import List from "./List";
 import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StrictMode>
+    <List />
+  </StrictMode>
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
