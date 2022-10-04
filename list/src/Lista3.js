11c69b1b9dcbae4518cab8bb8c3b56315178e738
Moisés R. Martins <90099815+moisesrmartins@users.noreply.github.com>
2022-10-04 15:56:33 -0300

    React_6_List Parte 4.4: Request e Success

--- list/src/Lista3.js
+++ list/src/Lista3.js
@@ -6,6 +6,15 @@ const url =
 
 const reducer = (state, action) => {
   console.log("state", state, "action", action);
+  if (action.type === "REQUEST") {
+    return {
+      ...state,
+      loading: true,
+    };
+  }
+  if (action.type === "SUCCESS") {
+    return { ...state, loading: false, data: action.data };
+  }
   return state;
 };
 
