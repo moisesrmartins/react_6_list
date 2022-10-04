da572169f961b5887fd3b76766762cbc1241e5dd
Moisés R. Martins <90099815+moisesrmartins@users.noreply.github.com>
2022-10-04 08:40:01 -0300

    React_6_List Parte 3.3: Loading

--- list/src/Lista2.js
+++ list/src/Lista2.js
@@ -5,14 +5,21 @@ const url =
   "https://myproject-be4c4-default-rtdb.firebaseio.com/lista-dois.json";
 
 function Lista2() {
+  const [loading, setLoading] = useState(false);
   const [data, setData] = useState({});
 
   useEffect(() => {
+    setLoading(true);
     axios.get(url).then((res) => {
       setData(res.data);
       console.log(res.data);
+      setLoading(false);
     });
   }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <div className="Lista2">
