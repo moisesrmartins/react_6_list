debcc443e6afec5cb06678b41e2fe00f512c9b21
Moisés R. Martins <90099815+moisesrmartins@users.noreply.github.com>
2022-10-04 10:39:06 -0300

    React_6_List Parte 4.3: useReducer, dispatch e loading

--- list/src/Lista3.js
+++ list/src/Lista3.js
@@ -1,19 +1,32 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useReducer } from "react";
 import axios from "axios";
 
 const url =
   "https://myproject-be4c4-default-rtdb.firebaseio.com/lista-teste.json";
 
+const reducer = (state, action) => {
+  console.log("state", state, "action", action);
+  return state;
+};
+
 function Lista3() {
+  const [data, dispatch] = useReducer(reducer, { loading: true, data: {} });
+
   useEffect(() => {
+    dispatch({ type: "REQUEST" });
     axios.get(url).then((res) => {
-      console.log(res.data);
+      dispatch({ type: "SUCCESS", data: res.data });
     });
   }, []);
+
+  if (data.loading) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <div className="Lista3">
       <h1>Lista-Teste</h1>
+      {JSON.stringify(data)}
     </div>
   );
 }
