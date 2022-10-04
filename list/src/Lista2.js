6f39409942c85c77fd14ce87b75387f675c0038a
Moisés R. Martins <90099815+moisesrmartins@users.noreply.github.com>
2022-10-04 08:32:04 -0300

    React_6_List Parte 3.2: useState

--- list/src/Lista2.js
+++ list/src/Lista2.js
@@ -1,12 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const url =
   "https://myproject-be4c4-default-rtdb.firebaseio.com/lista-dois.json";
 
 function Lista2() {
+  const [data, setData] = useState({});
+
   useEffect(() => {
     axios.get(url).then((res) => {
+      setData(res.data);
       console.log(res.data);
     });
   }, []);
@@ -14,6 +17,7 @@ function Lista2() {
   return (
     <div className="Lista2">
       <h1>Lista-Dois</h1>
+      {JSON.stringify(data)}
     </div>
   );
 }
