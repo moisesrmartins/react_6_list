83b551fb352877633dda754294576a32736f5d7c
Moisés R. Martins <90099815+moisesrmartins@users.noreply.github.com>
2022-10-04 10:35:33 -0300

    React_6_List Parte 4.2: Axios, useEffect e url

--- list/src/Lista3.js
+++ list/src/Lista3.js
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
+
+const url =
+  "https://myproject-be4c4-default-rtdb.firebaseio.com/lista-teste.json";
 
 function Lista3() {
+  useEffect(() => {
+    axios.get(url).then((res) => {
+      console.log(res.data);
+    });
+  }, []);
+
   return (
     <div className="Lista3">
       <h1>Lista-Teste</h1>
